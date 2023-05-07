870c54403eb096be91158cebe3d98d0ca937f7bc
Requiem98 <60062428+Requiem98@users.noreply.github.com>
2023-05-07 12:48:29 +0200

    Add files via upload

---
+++ aggiorna.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#Questo script aggiorna la directory di github (~/Documenti/GitHub_projects/Workspaces)
+os=`uname`
+pcname=`hostname`
+
+if [ $os == "MINGW64_NT-10.0-19044" ]
+then
+	cd /c/Users/amede/GitHub_projects/Workspaces/
+fi
+
+#Controlla che non ci siano aggiornamenti e poi carica le modifiche fatte nello stage
+git pull origin master
+git add *
+
+#Controlla che non siano stati eliminati dei file dalla directory
+log=`git commit -m "aggiornato da $pcname" | awk '/eliminato:/{print $2}'`
+
+#se li trova li elimina da git
+for i in $log
+	do
+		git rm $i; git commit -m "aggiornato da $pcname"
+done
+
+#stesso codice ma per il terminale inglese
+log=`git commit -m "aggiornato da $pcname" | awk '/deleted:/{print $2}'`
+
+#se li trova li elimina da git
+for i in $log
+	do
+		git rm $i; git commit -m "aggiornato da $pcname"
+done
+
+#aggiorna la directory
+git push origin master
+
+read -n1 -r -p "Press any key to continue..."
+
+
+#Token di accesso alla directory: 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
+
+
+#Codice per clonare repository: git clone https://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@github.com/Requiem98/Workspaces.git
